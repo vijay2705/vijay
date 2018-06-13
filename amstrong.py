0ed3d5a7d238d64596fe09c07e32907ace50f2ea
vijay2705 <34597078+vijay2705@users.noreply.github.com>
2018-06-13 10:45:14 +0530

    Create amstrong.py

---
+++ amstrong.py
@@ -0,0 +1,12 @@
+a=input()
+lenght=len(num)
+sum=0
+for i in range(0,l):
+    x=num[i]
+    y=(int(x))**3
+    sum +=b
+if sum==int(num):
+    print("it is an amstrong no")
+else:
+    print("it is not an amstrong no")
+    
